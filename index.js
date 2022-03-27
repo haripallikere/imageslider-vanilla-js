354d2e1a94cd830d1fcad59e1f2bbc17cd3dafc5
Harikrishna Pallikere <42885238+haripallikere@users.noreply.github.com>
2022-03-27 16:12:44 +0530

    v-1

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-let imageIndex = ["media/0.webp", "media/1.webp", "media/2.webp"];
+let imageIndex = [
+  "media/0.webp",
+  "media/1.webp",
+  "media/2.webp",
+  "media/lachlan-dempsey-6VPEOdpFNAs-unsplash.jpg",
+  "media/ben-sweet-2LowviVHZ-E-unsplash.jpg",
+];
 
 let navigate = slideImages();
+
 function dotSelect(f) {
   generateImageTag(f);
   dotCheck(f);
@@ -8,6 +15,7 @@ function dotSelect(f) {
 
 function slideImages() {
   let flag = 0;
+  //logic for slides
   return (input) => {
     if (input < 0) {
       if (flag === 0) {
@@ -24,6 +32,7 @@ function slideImages() {
       }
     }
     generateImageTag(flag);
+    dotCheck(flag);
   };
 }
 
@@ -39,7 +48,6 @@ function generateImageTag(input = 0) {
 
 function generateDotNav() {
   let dotNav = document.querySelector(".dot-nav");
-  const dot = document.createElement("li");
   imageIndex.map((element, i) => {
     const dot = document.createElement("div");
     dot.setAttribute("class", "dots");
@@ -49,16 +57,21 @@ function generateDotNav() {
   });
 }
 
-function dotCheck(f) {
-  let check = document.getElementById(`${f}`);
-  if (check.classList.contains("active")) {
-    check.classList.remove("active");
-  }
+function dotCheck(value) {
+  let checkParent = document.querySelector(".dot-nav");
+  let check = document.getElementById(`${value}`);
+  let length = checkParent.children.length;
 
-  //   check.classList.add("active");
+  for (let i = 0; i < length; i++) {
+    if (checkParent.children[i].classList.contains("active")) {
+      checkParent.children[i].classList.remove("active");
+    }
+  }
+  check.classList.add("active");
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   generateImageTag();
   generateDotNav();
+  dotCheck(0);
 });
